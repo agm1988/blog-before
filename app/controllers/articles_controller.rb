fb94b7aeae2456171bd1d168aa6c9cf6288f45b6
Alexandr <Gsashko@mail.ru>
2013-01-11 11:22:45 +0200

    +sub

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,13 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def create_auth
+    #auth = env["omniauth.auth"]
+    #user = User.from_omniauth(auth)
+    #session[:user_id] = user.id
+    #redirect_to root_url, :notice => "Signed in successfully!"
+
+    raise env["omniauth.auth"].to_yaml
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Blog::Application.routes.draw do
-  resources :articles
-  resources :sessions
+
+  match "auth/:provider/callback", :to => "articles#create_auth"
   root to: 'articles#index'
+  resources :articles
+
+constraints(Subdomain) do
+  resources :sessions
 
   #match "auth/google_oauth2/callback", :to => "sessions#create"
   match "auth/failure", :to => "sessions#failure"
-  match "auth/:provider/callback", :to => "sessions#create"
+  #match "auth/:provider/callback", :to => "sessions#create"
   match "logout", :to => "sessions#destroy", :as => :logout
+end
 end
